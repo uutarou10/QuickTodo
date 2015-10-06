f768629e1665ed37d3e1eb3b28c1c8b76e56cb92
Kota Nonaka <kouta.nonaka@gmail.com>
2015-10-06 20:49:04 +0900

    add

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php include('include/header.php'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="text-center">
+                <h2>Login</h2>
+                <p>ログイン情報を入力してください。</p>
+                <form action="include/login.php" method="post">
+                    <div class="form-group">
+                        <label class="control-label">User ID</label>
+                        <input type="text" class="form-control" placeholder="ID" name="id">
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">Password</label>
+                        <input type="password" class="form-control" placeholder="PASSWORD" name="pass">
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="LOGIN" class="btn btn-primary">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
