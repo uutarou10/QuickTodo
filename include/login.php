6153ccc2f13e353de85fd0cbd7f0728b28d35295
Kota Nonaka <kouta.nonaka@gmail.com>
2015-10-06 20:10:15 +0900

    add
    ログイン機能

---
+++ include/login.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+if (isset($_SESSION['id'])) {
+    //ログイン状態だったらindex.phpに移動
+    header('Location: ../index.php');
+    exit();
+} elseif (isset($_POST['id']) && isset($_POST['pass'])) {
+    //ログインしてないけどidとパスワードが送られてきたら
+    include('db_connect.php');
+    $stmt = $db->prepare("
+    select * from users where user_id=:id and password=:pass;
+    ");
+    $stmt->bindParam(':id',$_POST['id'],PDO::PARAM_STR);
+    $stmt->bindParam(':pass',sha1($_POST['pass']));
+    $stmt->execute();
+
+    $row = $stmt->fetch();
+    if(isset($row)){
+        $_SESSION['id'] = $row['id'];
+    }
+    header('Location: ../success.php');
+    exit();
+} else {
+    //ログイン情報が間違っていたらログイン画面へ遷移
+    header('Location: ../login.php');
+    exit();
+}
+ ?>
+
