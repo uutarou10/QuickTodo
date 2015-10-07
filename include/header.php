35ae79fa56e2f23812b8d33335f34c91491b845b
Kota Nonaka <kouta.nonaka@gmail.com>
2015-10-07 15:08:54 +0900

    [change]デザイン変更

--- include/header.php
+++ include/header.php
@@ -29,7 +29,7 @@ if (isset($_SESSION['id'])) {
 </head>
 
 <body class="no-thank-yu">
-    <nav class="navbar navbar-inverse">
+    <nav class="navbar navbar-default">
         <div class="container-fluid">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 include('include/header.php');
  ?>
- <div class="container">
-     <h1>Welcome to QuickTodo!!</h1>
-     <p>The most simple todo in the world.</p>
+ <div class="jumbotron">
+     <div class="container">
+         <h1>Welcome to QuickTodo!!</h1>
+         <p>The most simple todo in the world.</p>
+     </div>
+ </div>
 
+ <div class="container">
      <div class="row">
          <div class="col-sm-7">
              <h2>How to use?</h2>
@@ -14,21 +18,23 @@ include('include/header.php');
              <p>これであなたもいけいけキャンパスライフを送ろう。</p>
          </div>
          <div class="col-sm-4">
-             <h2>Login</h2>
-             <p>ログイン情報を入力してください。</p>
-             <form action="include/login.php" method="post">
-                 <div class="form-group">
-                     <label class="control-label">User ID</label>
-                     <input type="text" class="form-control" placeholder="ID" name="id">
-                 </div>
-                 <div class="form-group">
-                     <label class="control-label">Password</label>
-                     <input type="password" class="form-control" placeholder="PASSWORD" name="pass">
-                 </div>
-                 <div class="form-group">
-                     <input type="submit" value="LOGIN" class="btn btn-primary">
-                 </div>
-             </form>
+             <div class="well bs-component">
+                 <h2>Login</h2>
+                 <p>ログイン情報を入力してください。</p>
+                 <form action="include/login.php" method="post">
+                     <div class="form-group">
+                         <label class="control-label">User ID</label>
+                         <input type="text" class="form-control" placeholder="ID" name="id">
+                     </div>
+                     <div class="form-group">
+                         <label class="control-label">Password</label>
+                         <input type="password" class="form-control" placeholder="PASSWORD" name="pass">
+                     </div>
+                     <div class="form-group">
+                         <input type="submit" value="LOGIN" class="btn btn-primary">
+                     </div>
+                 </form>
+             </div>
          </div>
      </div>
      <div class="row">
