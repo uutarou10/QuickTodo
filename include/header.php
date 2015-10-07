4dea7ae303a48d111033e69480e5b3eed63cc2bf
Kota Nonaka <kouta.nonaka@gmail.com>
2015-10-07 12:08:52 +0900

    [fix]リンク先を訂正

--- include/header.php
+++ include/header.php
@@ -37,7 +37,7 @@ if (isset($_SESSION['id'])) {
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">QuickTodo</a>
+                <a class="navbar-brand" href="index.php">QuickTodo</a>
             </div>
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
