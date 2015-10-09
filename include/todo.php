45455534d6c244bad2a66dbd2ee7dc1cde7acaf8
Kota Nonaka <kouta.nonaka@gmail.com>
2015-10-09 18:40:19 +0900

    [add]ユーザーのタスクを取得する関数を追加

---
+++ include/todo.php
@@ -0,0 +1,39 @@
+<?php
+function getTodos($id,$status) {
+    include('db_connect.php');
+    //$idは投稿したユーザーのid
+    //$statusは完了・未完了（0が未完了、1が完了）
+    $stmt=$db->prepare("
+    select * from todos where user_id=:id and status=:status order by id;
+    ");
+    $stmt->bindParam(':status',$status,PDO::PARAM_INT);
+    $stmt->bindParam(':id',$id,PDO::PARAM_INT);
+    $stmt->execute();
+
+    $rows = false;
+    while($row=$stmt->fetch()) {
+        $rows[] = $row;
+    }
+
+    return $rows;
+}
+
+function getAllTodos($id) {
+    include('db_connect.php');
+    //$idは投稿したユーザーのid
+    //すべてのステータスのタスクを取得する。
+    $stmt=$db->prepare("
+    select * from todos where user_id=:id order by id;
+    ");
+    $stmt->bindParam(':id',$id,PDO::PARAM_INT);
+    $stmt->execute();
+
+    $rows = false;
+    while($row=$stmt->fetch()) {
+        $rows[] = $row;
+    }
+
+    return $rows;
+}
+
+ ?>
